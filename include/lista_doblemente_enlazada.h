849262ec87429fda7b0f155188fbbbefc3d428d7
vboxuser <bsanchezchavarria@gmail.com>
2026-07-01 00:42:46 +0000

    Añadir Listas Dobles Enlazadas

--- include/lista_doblemente_enlazada.h
+++ include/lista_doblemente_enlazada.h
@@ -1,4 +1,26 @@
 #ifndef lista_doblemente_enlazada_h
 #define lista_doblemente_enlazada_h
+typedef struct Node {
+	int date;
+	struct Node *next;
+	struct Node *prev;
+} Node; //el nodo funciona como punto de conexion entre la memoria y los archivos
+
+typedef struct {
+	Node *head;
+	Node *tail;
+} DoublyLinkedList; //aca se procesan las listas
+
+DoublyLinkedList *createList();//aca se crean las listas con las que se cuenta
+
+void insertAtBeginning(DoublyLinkedList *, int data);
+void insertAtPosition(DoublyLinkedList *list, int data, int position);
+void deleteElement(DoublyLinkedList *list, int data); //borrar los datos previene  memory leaks
+Node *searchElement(DoublyLinkedList *list, int data);
+void printForward(DoublyLinkedList *list); //esto permite el chequeo de que las cosas funcionen 
+void printBackward(DoublyLinkedList *list);
+//se imprime tanto el fontral como posterior, para saber ern todo momento lo que esta pasando 
+void freeList(DoublyLinkedList *list);
+//esta ultima llamada busca volver a evitar los memory leaks y finalizar el proceso 
 
 #endif
